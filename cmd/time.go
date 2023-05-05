b9e55e7a1fe901cc385f2f32942fd64c6caa8a1d
李铭昕 <715557344@qq.com>
2023-05-05 20:15:18 +0800

    Support read from stdin

--- cmd/time.go
+++ cmd/time.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/fatih/color"
 	"github.com/logrusorgru/aurora"
@@ -17,29 +18,42 @@ var timeCmd = &cobra.Command{
 	Short: "格式化时间格式",
 	Long:  `格式化时间格式`,
 	Run: func(cmd *cobra.Command, args []string) {
-		headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+		var t string
 		if len(args) != 0 {
-			t := args[0]
-			var dt time.Time
-			if ts, err := strconv.ParseInt(t, 10, 64); err == nil {
-				dt = time.Unix(ts, 0)
-			} else {
-				dt, err = time.ParseInLocation(time.DateTime, t, zone)
-				if err != nil {
-					fmt.Println(aurora.Red(err))
-					os.Exit(1)
-				}
-			}
+			dump(args[0])
+		}
 
-			tbl := table.New("TimeStamp", "DateTime")
-			tbl.WithHeaderFormatter(headerFmt)
-			tbl.AddRow(dt.Unix(), dt.Format(time.DateTime))
-			tbl.Print()
+		in := bufio.NewReader(os.Stdin)
+		scanner := bufio.NewScanner(in)
+		for true {
+			fmt.Println(aurora.Blue("Please Input TimeStamp or Datetime:"))
+			scanner.Scan()
+			t = string(scanner.Bytes())
+			dump(t)
 		}
 	},
 }
 
 var zone = time.FixedZone("CST", 8*3600)
+
+func dump(val string) {
+	var dt time.Time
+	if ts, err := strconv.ParseInt(val, 10, 64); err == nil {
+		dt = time.Unix(ts, 0)
+	} else {
+		dt, err = time.ParseInLocation(time.DateTime, val, zone)
+		if err != nil {
+			fmt.Println(aurora.Red(err))
+			os.Exit(1)
+		}
+	}
+
+	tbl := table.New("TimeStamp", "DateTime")
+	tbl.WithHeaderFormatter(color.New(color.FgGreen, color.Underline).SprintfFunc())
+	tbl.AddRow(dt.Unix(), dt.Format(time.DateTime))
+	tbl.Print()
+	fmt.Println("")
+}
 
 func init() {
 	rootCmd.AddCommand(timeCmd)
