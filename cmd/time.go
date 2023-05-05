1cba9ccc947fb6fe5f9a98cec4816a1d1da5ab6e
李铭昕 <715557344@qq.com>
2023-05-05 18:48:57 +0800

    增加时间戳转换

--- cmd/time.go
+++ cmd/time.go
@@ -2,8 +2,13 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/fatih/color"
+	"github.com/logrusorgru/aurora"
+	"github.com/rodaine/table"
 	"github.com/spf13/cobra"
+	"os"
+	"strconv"
+	"time"
 )
 
 // timeCmd represents the time command
@@ -12,10 +17,29 @@ var timeCmd = &cobra.Command{
 	Short: "格式化时间格式",
 	Long:  `格式化时间格式`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("time called")
-		fmt.Println(args)
+		headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+		if len(args) != 0 {
+			t := args[0]
+			var dt time.Time
+			if ts, err := strconv.ParseInt(t, 10, 64); err == nil {
+				dt = time.Unix(ts, 0)
+			} else {
+				dt, err = time.ParseInLocation(time.DateTime, t, zone)
+				if err != nil {
+					fmt.Println(aurora.Red(err))
+					os.Exit(1)
+				}
+			}
+
+			tbl := table.New("TimeStamp", "DateTime")
+			tbl.WithHeaderFormatter(headerFmt)
+			tbl.AddRow(dt.Unix(), dt.Format(time.DateTime))
+			tbl.Print()
+		}
 	},
 }
+
+var zone = time.FixedZone("CST", 8*3600)
 
 func init() {
 	rootCmd.AddCommand(timeCmd)
