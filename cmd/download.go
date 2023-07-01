d750e3b51e2ba8a04c74287ac8258823e78ec874
李铭昕 <715557344@qq.com>
2023-07-01 15:44:16 +0800

    增加下载 m3u8 文件到本地

---
+++ cmd/download.go
@@ -0,0 +1,104 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/logrusorgru/aurora"
+	"github.com/spf13/cobra"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var downloadM3u8 = &cobra.Command{
+	Use:   "d:m3u8",
+	Short: "下载 m3u8 到本地",
+	Long:  "下载 m3u8 到本地",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println(aurora.Yellow("Please input the url or path about m3u8."))
+			os.Exit(1)
+		}
+
+		dir, _ := cmd.Flags().GetString("dir")
+		if dir == "" {
+			dir = "./output"
+		}
+
+		resp, err := http.Get(args[0])
+		if err != nil {
+			fmt.Errorf("文件下载失败 %s", err)
+			os.Exit(1)
+		}
+
+		defer resp.Body.Close()
+
+		bf := bufio.NewReader(resp.Body)
+		err = os.Mkdir(dir, os.ModePerm)
+		if err != nil {
+			fmt.Errorf("文件下载失败 %s", err)
+			os.Exit(1)
+		}
+
+		f, err := os.OpenFile(dir+"/index.txt", os.O_WRONLY|os.O_CREATE, os.ModePerm)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		w := bufio.NewWriter(f)
+
+		for {
+			line, _, err := bf.ReadLine()
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				fmt.Printf("文件读取失败 %s", err)
+				os.Exit(1)
+			}
+
+			url := string(line)
+
+			if strings.HasPrefix(url, "http") {
+				arr := strings.Split(url, "/")
+				file := arr[len(arr)-1]
+
+				download(url, dir+"/"+file)
+
+				w.WriteString("file '" + file + "'\n")
+			}
+		}
+
+		// ffmpeg -f concat -safe 0 -i index.txt -c copy output.mkv
+		w.Flush()
+	},
+}
+
+func download(url string, path string) {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	r, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer r.Body.Close()
+
+	io.Copy(f, r.Body)
+}
+
+func init() {
+	rootCmd.AddCommand(downloadM3u8)
+
+	downloadM3u8.Flags().StringP("dir", "D", "", "下载位置")
+}
