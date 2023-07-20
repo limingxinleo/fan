2c3397badf5d55b062e53657e5e031687c934d54
李铭昕 <715557344@qq.com>
2023-07-20 23:45:19 +0800

    优化 m3u8 下载脚本

--- cmd/download.go
+++ cmd/download.go
@@ -1,14 +1,12 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
-	"io"
-	"net/http"
 	"os"
-	"strings"
+	"os/exec"
 )
 
 var downloadM3u8 = &cobra.Command{
@@ -17,92 +15,31 @@ var downloadM3u8 = &cobra.Command{
 	Long:  "下载 m3u8 到本地",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
-			fmt.Println(aurora.Yellow("Please input the url or path about m3u8."))
+			fmt.Println(aurora.Yellow("Please input the url about m3u8."))
 			os.Exit(1)
 		}
 
-		dir, _ := cmd.Flags().GetString("dir")
-		if dir == "" {
-			dir = "./output"
+		name := uuid.New().String() + ".mp4"
+		cmd2 := exec.Command(
+			"ffmpeg",
+			"-allowed_extensions",
+			"ALL",
+			"-protocol_whitelist",
+			"'file,https,crypto,tcp,tls,http'",
+			"-i",
+			args[0],
+			"-vcodec",
+			"copy",
+			"-acodec",
+			"copy",
+			name,
+		)
+
+		e := cmd2.Run()
+		if e != nil {
+			fmt.Println(e)
 		}
-
-		resp, err := http.Get(args[0])
-		if err != nil {
-			fmt.Errorf("文件下载失败 %s", err)
-			os.Exit(1)
-		}
-
-		defer resp.Body.Close()
-
-		bf := bufio.NewReader(resp.Body)
-		err = os.Mkdir(dir, os.ModePerm)
-		if err != nil {
-			fmt.Errorf("文件下载失败 %s", err)
-			os.Exit(1)
-		}
-
-		f, err := os.OpenFile(dir+"/index.txt", os.O_RDWR|os.O_CREATE, os.ModePerm)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		w := bufio.NewWriter(f)
-
-		for {
-			line, _, err := bf.ReadLine()
-			if err == io.EOF {
-				break
-			}
-
-			if err != nil {
-				fmt.Printf("文件读取失败 %s", err)
-				os.Exit(1)
-			}
-
-			url := string(line)
-
-			if strings.HasPrefix(url, "http") {
-				fmt.Println(url)
-				arr := strings.Split(url, "/")
-				file := arr[len(arr)-1]
-
-				download(url, dir+"/"+file)
-
-				w.WriteString("file '" + file + "'\n")
-			}
-		}
-
-		// ffmpeg -f concat -safe 0 -i index.txt -c copy output.mkv
-		w.Flush()
 	},
-}
-
-func download(url string, path string) int64 {
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, os.ModePerm)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	defer f.Close()
-
-	r, err := http.Get(url)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	defer r.Body.Close()
-
-	written, err := io.Copy(f, r.Body)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println(written)
-	return written
 }
 
 func init() {
